29853cb80c0eccf66d404229a896830cf91c6a9c
Harsha Somisetty <harshasomisetty7@gmail.com>
2023-05-23 10:04:03 -0400

    content layer update

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -1,34 +1,39 @@
-import { defineDocumentType, makeSource } from "contentlayer/source-files";
+import { defineDocumentType, makeSource } from 'contentlayer/source-files';
 
 export const Blog = defineDocumentType(() => ({
-  name: "Blog",
+  name: 'Blog',
   filePathPattern: `**/*.mdx`,
-  contentType: "mdx",
+  contentType: 'mdx',
   fields: {
     title: {
-      type: "string",
-      description: "The title of the blog",
+      type: 'string',
+      description: 'The title of the blog',
       required: true,
     },
     date: {
-      type: "string",
-      description: "The date of the blog",
+      type: 'string',
+      description: 'The date of the blog',
       required: true,
+    },
+    description: {
+      type: 'string',
+      description: 'Post description',
+      required: false,
     },
   },
   computedFields: {
     url: {
-      type: "string",
+      type: 'string',
       resolve: (blog) => `/blog/${blog._raw.flattenedPath}`,
     },
     slug: {
-      type: "string",
+      type: 'string',
       resolve: (doc) => doc._raw.flattenedPath,
     },
   },
 }));
 
 export default makeSource({
-  contentDirPath: "src/content",
+  contentDirPath: 'src/content',
   documentTypes: [Blog],
 });
